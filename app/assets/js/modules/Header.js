b6df61d358f367fb2fa2b49f7de74f4dd57ce060
Anuar Jimenez <heyitsanuar@gmail.com>
2018-07-10 13:05:26 -0500

    Initial setup.

---
+++ app/assets/js/modules/Header.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+class Header {
+    
+    constructor(){
+        this.events();
+    }
+
+    events(){
+
+    }
+    
+}
+
+export default Header;
---
+++ gulp/tasks/path.js
@@ -0,0 +1,5 @@
+let DIST_PATH    = './dist/assets',
+	SCRIPTS_PATH = './app/assets/scripts/**/*.js',
+	SCRIPTS_TEMP = './app/temp/js/scripts.js',
+	CSS_PATH     = './app/assets/css/**/*.scss',
+	CSS_TEMP     = './app/temp/css/styles.css';
