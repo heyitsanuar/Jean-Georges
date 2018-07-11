2bcdc1f6e8c9684eb25b75eb12375dac4a104497
Anuar Jimenez <heyitsanuar@gmail.com>
2018-07-11 05:18:52 -0500

    Complete Mobile and Tablet version for About us.

--- app/assets/js/modules/Header.js
+++ app/assets/js/modules/Header.js
@@ -3,18 +3,23 @@ import $ from 'jquery';
 class Header {
     
     constructor(){
-        this.btnMenu = $("#btn-menu");
+        this.btnMenuOpen = $("#btn-menu-open");
+        this.btnMenuClose = $("#btn-menu-close");
+        this.banner = $("#banner-info");
         this.menu = $("#menu");
 
         this.events();
     }
 
     events(){
-        this.btnMenu.on("click", this.showMenu.bind(this));
+        this.btnMenuOpen.on("click", this.showMenu.bind(this));
+        this.btnMenuClose.on("click", this.showMenu.bind(this));
     }
 
     showMenu(){
         this.menu.toggleClass("header__menu--is-active");
+        this.banner.toggleClass("header__banner--is-toggled");
+        this.btnMenuClose.toggleClass("header__menu-exit--is-visible");
     }
     
 }
