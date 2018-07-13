d25e4ba74b5c8e2ad3cdc207271f7f2da5f3d4a5
Anuar Jimenez <heyitsanuar@gmail.com>
2018-07-13 03:40:00 -0500

    Complete header color change on scroll

--- app/assets/js/modules/Header.js
+++ app/assets/js/modules/Header.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class Header {
     
@@ -7,6 +8,10 @@ class Header {
         this.btnMenuClose = $("#btn-menu-close");
         this.banner = $("#banner-info");
         this.menu = $("#menu");
+        
+        this.header = $(".header__navigation");
+        this.headerWaypointTrigger = $(".landing-page");
+        this.createHeaderWaypoints();
 
         this.events();
     }
@@ -20,6 +25,23 @@ class Header {
         this.menu.toggleClass("header__menu--is-active");
         this.banner.toggleClass("header__banner--is-toggled");
         this.btnMenuClose.toggleClass("header__menu-exit--is-visible");
+    }
+
+    createHeaderWaypoints(){
+        console.log("Hey");
+        var thisPrev = this;
+        console.log(thisPrev);
+
+        new Waypoint({
+            element: this.headerWaypointTrigger[0],
+            handler: function(direction){
+                if(direction == 'down'){
+                    thisPrev.header.addClass("header__navigation--dark");
+                }else{
+                    thisPrev.header.removeClass("header__navigation--dark");
+                }
+            }
+        });
     }
     
 }
